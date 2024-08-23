aaa4999afe69b085133499a8fad64cb33c572c7c
Azizbek <azizbekxotamaliyev@gmmail.com>
2024-08-23 12:08:22 +0500

    header and footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,103 @@
 import { Fragment } from "react";
 import "./footer.scss";
+import Link from "next/link";
+import { Icons } from "@/svg-icons";
 
 const Footer = () => {
   return (
     <Fragment>
-      <footer className="footer">Footer</footer>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-container__top">
+            <ul>
+              Информация
+              <li>
+                <Link href="#">О компании</Link>
+              </li>
+              <li>
+                <Link href="#">Наши партнеры</Link>
+              </li>
+              <li>
+                <Link href="#">Блог</Link>
+              </li>
+              <li>
+                <Link href="#">О наших акциях и скидках</Link>
+              </li>
+              <li>
+                <Link href="#">Пользовательское соглашение</Link>
+              </li>
+            </ul>
+            <ul>
+              Поддержка и сервис
+              <li>
+                <Link href="#">Вопросы и ответы</Link>
+              </li>
+              <li>
+                <Link href="#">Отзывы</Link>
+              </li>
+              <li>
+                <Link href="#">Контакты</Link>
+              </li>
+              <li>
+                <Link href="#">Для партнеров</Link>
+              </li>
+            </ul>
+            <ul>
+              Каталог товаров
+              <li>
+                <Link href="#">Кирпичи и блоки</Link>
+              </li>
+              <li>
+                <Link href="#">Цемент и сухие смеси</Link>
+              </li>
+              <li>
+                <Link href="#">Изоляционные материалы</Link>
+              </li>
+              <li>
+                <Link href="#">Сантехника</Link>
+              </li>
+            </ul>
+            <ul>
+              Контакты
+              <li>
+                <Link href="#">Телефон: +998 (71) 200-7-002</Link>
+              </li>
+              <li>
+                <Link href="#">Ташкент, ул. Коратош, 106</Link>
+              </li>
+              {/* <li>
+              <Link href="#">Изоляционные материалы</Link>
+            </li>
+            <li>
+              <Link href="#">Сантехника</Link>
+            </li> */}
+            </ul>
+          </div>
+          <div className="footer-container__bottom">
+            <div className="footer-container__bottom__left">
+              <div className="socials">
+                <div className="social">
+                  <Icons.instgramm />
+                </div>
+                <div className="social">
+                  <Icons.telegramm />
+                </div>
+                <div className="social">
+                  <Icons.facebook />
+                </div>
+              </div>
+              <p>© 2022 ООО «Интернет магазин «Get it».</p>
+            </div>
+            <div className="footer-container__bottom__right">
+              <p>Мобильное приложение:</p>
+              <div className="apps">
+                <div className="app"><Icons.apple/></div>
+                <div className="app"><Icons.android/></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </Fragment>
   );
 };
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,36 @@
 import { Fragment } from "react";
+import Link from "next/link";
+
+import { Icons } from "@/svg-icons";
+import NavLink from "../navlink";
+
 import "./header.scss";
 
 const Header = () => {
   return (
     <Fragment>
-      <header className="header">Header</header>
+      <header className="header">
+        <div className="container header__container">
+          <div className="header__container__left">
+            <Link href="/" className="logo">Get it</Link>
+            <button className="catalog-btn">
+              <Icons.burger/> Каталог товаров
+            </button>
+            <div className="navs">
+              <NavLink href="/">Главная</NavLink>
+              <NavLink href="#">О нас</NavLink>
+              <NavLink href="#">Услуги</NavLink>
+              <NavLink href="#">Продукция</NavLink>
+              <NavLink href="#">Блог</NavLink>
+              <NavLink href="#">Контакты</NavLink>
+            </div>
+          </div>
+          <div className="header__container__right">
+            <button className="lang"><Icons.globus/> RU</button>
+            <Link className="phone" href="tel:+998908205575">+71 123-45-67</Link>
+          </div>
+        </div>
+      </header>
     </Fragment>
   );
 };
